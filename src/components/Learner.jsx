50452b601e17f5c85c1619b3acea970b9040d27a
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-09-19 17:32:18 -0400

    completed lab

---
+++ src/components/Learner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Score from './Score';
+
+function Learner({ learner }) {
+    return (
+        <div className="learnerInfo">
+            <h2>{learner.name}</h2>
+            <p>{learner.bio}</p>
+            <h3>Scores:</h3>
+            <ul>
+                {learner.scores.map((score, i) => (
+                    <li key={i}>
+                        <Score score={score} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Learner;
